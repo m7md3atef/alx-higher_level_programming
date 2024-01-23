58874b807bd30f3013890861495fb64463a78dde
m7md3atef <m7md3atef@yahoo.com>
2024-01-22 16:16:18 -0800

    ok

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = ()
-    for i in range(max(len(tuple_a), len(tuple_b))):
-        new_tuple += ((tuple_a[i] if i < len(tuple_a) else 0) +
-                       (tuple_b[i] if i < len(tuple_b) else 0),)
-    return new_tuple
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
