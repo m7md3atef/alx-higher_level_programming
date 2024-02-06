03c614b112136183e8f461cf445510b674c631ed
m7md3atef <m7md3atef@yahoo.com>
2024-02-05 16:41:44 -0800

    ok

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,4 +7,4 @@ def safe_function(fct, *args):
         return fct(*args)
     except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
-        return Non
+        return None
